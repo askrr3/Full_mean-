794b834a4510e127578238a599f2e0b0bdf50980
Allen <askrr3@gmail.com>
2016-10-03 18:37:06 -0400

    practice stuff

---
+++ test.js
@@ -0,0 +1,56 @@
+// var array = [3, 5, 'Dojo', 'rocks', 'Michael', 'Sensei'];
+// for(var i = 0; i < array.length; i++){
+//   console.log(array[i]);
+// }
+// //
+// var shoe = [3, 5, 'Dojo', 'rocks', 'Michael', 'Sensei'];
+// shoe.push(100);
+// console.log(shoe);
+// //
+// var pants = [3, 5, 'Dojo', 'rocks', 'Michael', 'Sensei'];
+// pants.push(['hello', 'world', 'javascript is fun']);
+// console.log(pants);
+// //
+// var sum = 0;
+// for(var i = 1; i < 501; i++){
+//   sum += i;
+// }
+// console.log(sum);
+// //
+// var nums = [1, 5, 90, 25, -3, 0];
+// var temp = 0
+// for(var i = 0; i < nums.length; i++){
+//   if (nums[i] < nums[i + 1]){
+//     temp = nums[i]
+//   }
+// }
+// console.log(temp);
+
+// //
+// var nums = [1, 5, 90, 25, -3, 0];
+// var sum = 0;
+// var avg = 0;
+// for(var i = 0; i < nums.length; i++){
+//   sum += (nums[i])
+// }
+// avg = sum/(nums.length)
+// console.log(avg);
+// //
+// var new_ninja = {
+//  name: 'Jessica',
+//  profession: 'coder',
+//  favorite_language: 'JavaScript',
+//  dojo: 'Dallas'
+// }
+// for(key in new_ninja){
+//   console.log(new_ninja[key])
+// }
+
+
+var first_variable = "Yipee I was first!";
+console.log(first_variable);
+first_variable = "Not anymore!!!";
+function firstFunc() {
+  console.log(first_variable);
+}
+console.log(first_variable);
